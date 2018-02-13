f43491fcab247e7fe2dc6e5ad9ef4bfceb4abf00
LPLemnij <lpl16@case.edu>
2018-02-13 18:10:55 -0500

    Added The M_Value function
    Check if it's correct. I believe it is.

--- State Functions.rkt
+++ State Functions.rkt
@@ -19,5 +19,27 @@
   (lambda (state a b)
     (MStateAssign (removeState a state) a b)))
 
+;getValue returns the value of a variable from a state
+(define getValue
+  (lambda (var state)
+    (cond
+      ((eq? var (caar state)) (car (cadr state)))
+      (else (getValue var (cons (cdar state) (cons (cdr (car (cdr state))) '())))))))
+
+;M_value According to the assignment the parser gives operations in prefix notation
+(define M_value
+  (lambda (lis state)
+    (cond
+      ((list? (car lis)) (M_value (car lis)))
+      ((digit? (car lis)) (M_digit (car lis))
+      ((eq? '+ (car lis)) (+ (M_value (cadr lis)) (M_value (caddr lis))))
+      ((eq? '- (car lis)) (- (M_value (cadr lis)) (M_value (caddr lis))))
+      ((eq? '* (car lis)) (* (M_value (cadr lis)) (M_value (caddr lis))))
+      ((eq? '/ (car lis)) (/ (M_value (cadr lis)) (M_value (caddr lis))))
+      ((eq? '% (car lis)) (modulo (M_value (cadr lis)) (M_value (caddr lis))))
+      (else (getValue (car lis) state))
+      
+    
+
 
 
