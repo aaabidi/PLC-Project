5de05c6111d6f9b4d5c8c4c57a2915f5a86c69b2
aaabidi <7968706+aaabidi@users.noreply.github.com>
2018-02-14 12:11:44 -0500

    Lookup and Small Edits
    I added a lookup function
    
    Luca, I changed some stuff to M_value. I added a close parenthesis on the line with digit and I added three close parentheses at the end of the whole function.

--- State Functions.rkt
+++ State Functions.rkt
@@ -2,11 +2,21 @@
 (define nextInState
   (lambda (state)
     (cons (cdar state) (cons (cdr(cadr state)) '()))))
+
 ;consing onto a state
 (define addToState
   (lambda (state a b)
     (cons (cons a (car state)) (cons (cons b (cadr state)) '()))))
 
+;finds the value of a variable given a state
+(define lookup
+  (lambda (name state)
+    (cond
+      ((null? (car state)) (error "Variable with specified name not found" name) )
+      ((equal? name (caar state)) (caar (cdr state)))
+      (else (lookup name (nextInState state))))))
+
+;removing a variable from the state
 (define removeState
   (lambda (x state)
     (cond
@@ -14,7 +24,7 @@
       ((eq? x (caar state)) (nextInState state))
       (else (addToState (removeState x (nextInState state)) (caar state) (caadr state))))))
 
-
+; adds a variable to the state
 (define MStateAssign
   (lambda (state a b)
     (MStateAssign (removeState a state) a b)))
@@ -31,15 +41,15 @@
   (lambda (lis state)
     (cond
       ((list? (car lis)) (M_value (car lis)))
-      ((digit? (car lis)) (M_digit (car lis))
+      ((digit? (car lis)) (M_digit (car lis)))
       ((eq? '+ (car lis)) (+ (M_value (cadr lis)) (M_value (caddr lis))))
       ((eq? '- (car lis)) (- (M_value (cadr lis)) (M_value (caddr lis))))
       ((eq? '* (car lis)) (* (M_value (cadr lis)) (M_value (caddr lis))))
       ((eq? '/ (car lis)) (/ (M_value (cadr lis)) (M_value (caddr lis))))
       ((eq? '% (car lis)) (modulo (M_value (cadr lis)) (M_value (caddr lis))))
-      (else (getValue (car lis) state))
+      (else (getValue (car lis) state)))))
       
-;Getting the value of a variable
+;Getting the decimal value of a number represented with each digit as an element in a list
 (define MValNum
   (lambda (num)
     (cond
