9e6c596bf5c53896ecdfb77d56a4b9a229d0b831
aaabidi <7968706+aaabidi@users.noreply.github.com>
2018-02-17 01:02:12 -0500

    toBoolean and toValue
    I added toBoolean and toValue functions to be used in M_State to determine how to evaluate the next term in the statement list

--- Interpreter.scm
+++ Interpreter.scm
@@ -30,7 +30,7 @@
 ;      ((eq? var (caar state)) (car (cadr state)))
 ;      (else (getValue var (cons (cdar state) (cons (cdr (car (cdr state))) '())))))))
 
-;M_value According to the assignment the parser gives operations in prefix notation
+
 ;M_value According to the assignment the parser gives operations in prefix notation
 (define M_value
   (lambda (lis state)
@@ -43,8 +43,11 @@
       ((eq? '* (car lis)) (* (M_value (mlist(cadr lis)) state) (M_value (mlist(caddr lis)) state)))
       ((eq? '/ (car lis)) (/ (M_value (mlist(cadr lis)) state) (M_value (mlist(caddr lis)) state)))
       ((eq? '% (car lis)) (modulo (M_value (mlist(cadr lis)) state) (M_value (mlist(caddr lis)) state)))
+      ;Check for a letter and then go to lookup, but the else case should go all the way back to M_state
       (else (lookup (car lis) state)))))
 ;(M_value '(+ (* 3 x) 4) '((x) (5)))
+
+
 
 ;(M_boolean '(> (* x (+ x x)) y) ((x y) (3 50)))
 ;(M_boolean '(== true (>= (+ 3 7) (+ 4 10))) '(()()))
@@ -63,10 +66,9 @@
       ((eq? '|| (car lis)) (or (M_boolean (mlist(cadr lis)) state) (M_boolean (mlist(caddr lis)) state)))
       ((eq? '!  (car lis)) (not (M_boolean (mlist(cadr lis)) state) (M_boolean (mlist(caddr lis)) state)))
       ((eq? 'true (car lis)) #t)
-      ((eq? 'false (car lis)) #f)
+      ((eq? 'false (car lis)) #f)i
+      ;CHANGE THIS TO M_STATE WHEN THAT FUNCTION IS COMPLETED
       (else (M_value lis state)))))
-      
-      
       
                   
 ;Getting the decimal value of a number represented with each digit as an element in a list
@@ -75,11 +77,12 @@
     (cond
       ;returning 0 might be a bad idea
       ((null? num) '0)
-      (else (+ (MValNum (cdr num)) (* (car num) (expt 10 (- (length num) 1))) )) )))    
+      (else (+ (MValNum (cdr num)) (* (car num) (expt 10 (- (length num) 1))))))))    
 ;Tests for M_valNum
 ;(M_valNum '())
 ;(M_valNum '(3 5 9))
 ;(M_valNum '(0 1 0 0 1 0))
+
 
 ;finds the value of a variable given a state
 (define M_valVar
@@ -91,6 +94,7 @@
 ;Tests for M_valVar
 ;(M_valVar 'c '((a b c)(1 2 3)))
 ;(M_valVar 'z '((a b c)(1 2 3)))
+
 
 ;finds the value of a variable given a state
 (define lookup
@@ -106,3 +110,33 @@
     (cons x '())))
 
 
+(define toBoolean?
+  (lambda (lis)
+    (cond
+      ((null? lis) (error "input not a valid statement" lis))
+      ((eq? '== (car lis)) (#t))
+      ((eq? '!= (car lis)) (#t))
+      ((eq? '< (car lis)) (#t))
+      ((eq? '> (car lis)) (#t))
+      ((eq? '<= (car lis)) (#t))
+      ((eq? '>= (car lis)) (#t))
+      ((eq? '&& (car lis)) (#t))
+      ((eq? '|| (car lis)) (#t))
+      ((eq? '! (car lis)) (#t))
+      ((eq? 'true (car lis)) (#t))
+      ((eq? 'false (car lis)) (#t)))))
+
+
+(define toValue?
+  (lambda (lis)
+    (cond
+      ((null? lis) (error "input is not a valid speaker"))
+      ((eq? '+ (car lis)) (#t))
+      ((eq? '- (car lis)) (#t))
+      ((eq? '* (car lis)) (#t))
+      ((eq? '/ (car lis)) (#t))
+      ((eq? '% (car lis)) (#t))
+      ((eq? '+ (car lis)) (#t))
+      ((eq? '+ (car lis)) (#t))
+      ((eq? '+ (car lis)) (#t)))))
+      
